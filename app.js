f234de7ea016994d35f64259edc9dfe51e27221b
ManeeshWije <m.mwije1@gmail.com>
2021-06-21 19:08:21 -0400

    need to get fetch working

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const fetch = require("node-fetch");
 const prompt = require("prompt-sync")({ sigint: true });
-const APIkey = "501889bf-4d9e-4ed7-beaa-b4b24cbf693c";
+// const APIkey = "501889bf-4d9e-4ed7-beaa-b4b24cbf693c";
 
-fetch(
-  "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/process?key=APIkey"
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let newWord = data[0].meta.syns[0][0];
-  });
+let text = prompt("Enter piece of text: ");
+let result = text.split(" ");
+
+for (let i = 0; i < result.length; i++) {
+  let word = result[i];
+  if (word.length > 7) {
+    fetch(
+      `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/parameter?key=501889bf-4d9e-4ed7-beaa-b4b24cbf693c`
+    )
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+}
+
+// w
+//
+//
